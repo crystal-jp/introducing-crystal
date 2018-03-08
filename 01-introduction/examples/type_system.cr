4d58344809b9256b8999aecbc828d71159048a35
Hirofumi Wakasugi <baenej@gmail.com>
2018-03-08 10:48:05 +0900

    Chapter 1 first draft

---
+++ 01-introduction/examples/type_system.cr
@@ -0,0 +1,10 @@
+# tag::code[]
+def shout(x)
+  # Int32 と String はどちらも `to_s` メソッドを持っている
+  x.to_s.upcase
+end
+
+foo = ENV["FOO"]? || 10
+
+typeof(foo) # => (Int32 | String)
+typeof(shout(foo)) # => String
