6d5e2b7d8bbe45e96917dd46b30d937298fc3169
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2018-09-15 07:23:58 +0900

    splat展開の説明を修正

--- 04-macro/examples/syntax/splat.cr
+++ 04-macro/examples/syntax/splat.cr
@@ -1,7 +1,35 @@
-macro splat(*args)
-  print {{*args}}, "\n"
+macro splat
+  p "Splatting a array"
+  {% array = [1, 2, 3] %}
+  p {{*array}}
+
+  p "Splatting a tuple"
+  {% tuple = {4, 5, 6} %}
+  p {{*tuple}}
+
+  # p "Double Splatting a hash"
+  # {% hash = {"a" => 1, "b" => 2} %}
+  # p {{**hash}}
+  # ------------
+  # Syntax error in expanded macro: splat:11: unexpected token: =>
+  #
+  # p "a" => 1, "b" => 2
+  #       ^
+
+  p "Double Splatting a named tuple"
+  {% named_tuple = {"c": 3, "d": 4} %}
+  p {{**named_tuple}}
 end
 
-splat 1, 2, 3
+splat
 # output:
-# 123
+# "Splatting a array"
+# 1
+# 2
+# 3
+# "Splatting a tuple"
+# 4
+# 5
+# 6
+# "Double Splatting a named tuple"
+# {c: 3, d: 4}
