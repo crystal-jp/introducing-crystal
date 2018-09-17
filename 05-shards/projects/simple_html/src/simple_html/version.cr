56170ff40113d0c6c1a146b467936be2151695f8
ʕ·ᴥ·ʔAKJ <arcage@denchu.org>
2018-09-17 11:42:19 +0900

    Crystal 0.26.1 対応
    プロジェクトひな形の構成が変わっていたのの対応

--- 05-shards/projects/simple_html/src/simple_html/version.cr
+++
@@ -1,3 +0,0 @@
-module SimpleHtml
-  VERSION = "0.1.0"
-end
--- 05-shards/projects/simple_html/src/simple_html_org.cr
+++ 05-shards/projects/simple_html/src/simple_html_org.cr
@@ -1,6 +1,6 @@
-require "./simple_html/*"
-
 # TODO: Write documentation for `SimpleHtml`
 module SimpleHtml
+  VERSION = "0.1.0"
+
   # TODO: Put your code here
 end
